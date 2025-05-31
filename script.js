dedff06a006ad24bd30a3e85d69b8bddf375abcd
Emiliyan247 <emiliyankazandzhiev@gmail.com>
2025-05-31 19:59:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ const comments = [
     author: "Паисий Хилендарски",
     quote: "„О, неразумни юроде! Поради что се срамиш да се наречеш българин?“",
     interpretation: "Днес това е призив да се гордеем с българската си идентичност.",
-    image: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Paisij_Hilendarski.jpg"
+    image: "images/paisii.jpg"
   },
   {
     author: "Георги С. Раковски",
     quote: "„Българийо, за тебе те умряха...“",
     interpretation: "Жертвата на героите има смисъл, ако сме достойни за нея. Това ни задължава да изграждаме по-справедливо и честно общество. Държавата трябва да отразява достойнството на народа си.",
-    image: "https://upload.wikimedia.org/wikipedia/commons/6/6e/Georgi_Sava_Rakovski.jpg"
+    image: "images/rakowski.jpg"
   },
   {
     author: "Любен Каравелов",
     quote: "„Ако спечеля, печели цял народ – ако загубя, губя само мене си.“",
     interpretation: "Истинският лидер действа в името на всички, не само за себе си. Днес това е пример за лична отговорност и гражданска доблест. Рисковете в името на общото благо са най-стойностни.",
-    image: "https://upload.wikimedia.org/wikipedia/commons/e/e2/Lyuben_Karavelov.jpg"
+    image: "images/karawelow.jpg"
   },
   {
     author: "Христо Ботев",
@@ -27,13 +27,13 @@ const comments = [
     author: "Васил Левски",
     quote: "„Времето е в нас и ние сме във времето; то нас обръща и ние него обръщаме.“",
     interpretation: "Левски ни напомня, че ние не сме безсилни пред обстоятелствата. Можем да променяме бъдещето чрез действията си днес. Всеки човек има значение в хода на историята.",
-    image: "https://upload.wikimedia.org/wikipedia/commons/b/bf/Vasil_Levski_portrait.jpg"
+    image: "images/lewski.jpg"
   },
   {
     author: "Добри Чинтулов",
     quote: "„Стани, стани, юнак балкански!“",
     interpretation: "Това е зов за пробуждане и действие. Днес означава да не сме пасивни пред неправдата и бездействието. Всеки от нас може да бъде „юнак“ – чрез знания, доблест и активна позиция.",
-    image: "https://upload.wikimedia.org/wikipedia/commons/1/1a/Dobri_Chintulov.jpg"
+    image: "images/Chintilow.jpg"
   }
 ];
 
